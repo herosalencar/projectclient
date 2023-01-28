8e43b47151992e400d23755f57a5eeafa0a8eb83
herosalencar <contato@herosalencar.dev.br>
2023-01-28 20:03:25 -0300

    Update and Delete Client

--- src/main/java/com/example/projectclient/services/ClientService.java
+++ src/main/java/com/example/projectclient/services/ClientService.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.projectclient.dto.ClientDTO;
 import com.example.projectclient.entities.Client;
 import com.example.projectclient.repositories.ClientRepository;
-import com.example.projectclient.services.exceptions.EntityNotFoundException;
+import com.example.projectclient.services.exceptions.ResourceNotFoundException;
+import com.example.projectclient.services.exceptions.DatabaseException;
 
 @Service
 public class ClientService {
@@ -28,7 +31,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found!"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
 		return new ClientDTO(entity);
 	}
 	
@@ -38,6 +41,30 @@ public class ClientService {
 		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+	
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		} catch (ResourceNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+		
 	}
 	
 	private void copyDtoToEntity(ClientDTO dto, Client entity) {
